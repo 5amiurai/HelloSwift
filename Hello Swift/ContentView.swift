ff1c2f06a7074e90373eaeb0c3da7acea216cd17
5amiurai <sami.rizque@gmail.com>
2025-11-09 14:04:33 -0700

    Initial Commit

---
+++ Hello Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello Swift
+//
+//  Created by Sami Rizk on 2025-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello Swift/Hello_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftApp.swift
+//  Hello Swift
+//
+//  Created by Sami Rizk on 2025-11-09.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
